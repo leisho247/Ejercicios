fcd78cf0dc99573126a02475cf9a7f42091e3317
leisho247 <nogalesleo123@gmail.com>
2024-06-25 01:51:22 -0300

    ejercicios SQL

---
+++ clase17/ejercicios/queries.sql
@@ -0,0 +1,12 @@
+
+
+Cuantas tiendas (store) hay en total ?
+-- hay 2 tiendas 
+Cuantas peliculas (film) tiene una duracuon de mas de 120 minutos?
+-- 457 peliculas
+Cuantos pagos realizo el clientre (custore) con el ID 472?
+-- 26 pagos realizados 
+Cual es el ID de la pelicula (film) que tiene un iD de invetario (inventory) 303 ?
+-- es el ID 68
+Cual es la direccion de (address) y distrito (district) de la persona cuyo telefono es 634445428822 ?
+-- la direccion es 1565 Tangagil Manor del distrito Okinawa
---
+++ clase18/ejercicios/queriesJoin.sql
@@ -0,0 +1,12 @@
+
+
+Contar cuantas peliculas empiezan con la letra "S"
+-- 119 peliculas
+Sumar la duracion en minutos de todas las peliculas clasificadas para "PG-13".
+-- 26859 minutos
+Mostrar todos los emails de los clientes junto con la suma de todas las peliculas que rentaron.
+--  599 emails de clientes rentaron mas de 10 peliculas 
+Mostrar el numero de telefono de la direccion que tiene el codigo postal grande .
+--el numero de telefono mas grande es 934730187245
+Mostrar la direccion , el distrito y el nombre de la ciudad del primer cliente inactivo
+-- 360 Toulouse Parkway , England , Southend-onSea  es el primer cliente inactivo
